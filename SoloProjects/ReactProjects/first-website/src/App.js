7b4018828d4a10d9452792af6a4d364e894fbd52
aidangoldie7 <40402670@live.napier.ac.uk>
2022-12-27 12:45:10 +0000

    1. Initial Commit

---
+++ SoloProjects/ReactProjects/first-website/src/App.js
@@ -0,0 +1,39 @@
+import {createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import './App.css';
+
+//intial commit
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#2e1667",
+    },
+    secondary: {
+      main: "#c7d8ed",
+    },
+  },
+
+})
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p> 
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
